ef217b7a935542b95ed732c694317b32dfeb5a00
asimali-dev <asimalim106@gmail.com>
2025-11-01 00:44:36 +0500

    Initial commit

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import jsonServer from "json-server";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
+
+// ✅ yahan apni JSON file ka naam likho — exactly jaisa hai
+const router = jsonServer.router(path.join(__dirname, "Product-list.json"));
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-jsonServer.router("product-list.json")
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
+
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
 
 export default server;
